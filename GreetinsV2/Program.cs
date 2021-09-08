4532ba1bd1f0e1b2e2a7e5773ebe9a9a2b1f8a8e
Opilane TTHK <opilane@tthk.ee>
2021-09-08 08:39:04 +0300

    greetings

---
+++ GreetinsV2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreetinsV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja eesnime
+            //programm küsib kasutaja perekonnanime
+            //programm tervitab kasutajat, kasutades neid andmeid
+            Console.WriteLine("Mis su nimi on?");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Mis su perekonnanimi on?");
+            string LastName = Console.ReadLine();
+            Console.WriteLine($"Hello, {FirstName}, {LastName}");
+            Console.ReadLine();
+        }
+    }
+}
